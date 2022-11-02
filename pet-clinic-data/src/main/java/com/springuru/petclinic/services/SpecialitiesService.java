17b4f4a537ce51f9c31af93767ec4a0046a5485d
narjeswehbe <narjeswehbi04@gmail.com>
2022-11-02 14:23:19 +0200

    added SpecialitiesMapService

---
+++ pet-clinic-data/src/main/java/com/springuru/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.springuru.petclinic.services;
+
+import com.springuru.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springuru/petclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,33 @@
+package com.springuru.petclinic.services.map;
+
+import com.springuru.petclinic.model.Speciality;
+import com.springuru.petclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public Speciality save(Speciality obj) {
+        return super.save(obj);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
